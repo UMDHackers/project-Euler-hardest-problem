d670fee3c88925baddb6a09a63f62bf8956c18ef
Parth Desai <pdesai@fc-md.umd.edu>
2015-11-20 13:25:48 -0500

    Ok something is wrong with the SSL connections, trying to fix it lol!

--- project_euler.rb
+++ project_euler.rb
@@ -1,13 +1,30 @@
 require 'net/http'
 require 'open-uri'
-def return_pages(url)
-	source = Net::HTTP.get('stackoverflow.com', '/index.html')
-	return source
+require 'open_uri_redirections'
+def return_pages(uri)
+	http = Net::HTTP.new("www.projecteuler.com", 83)
+	http.use_ssl = true
+	http.ssl_version = :SSLv3
+	puts "?hello"
+	http.start { 
+		#uri2 = URI.parse(URI.escape(uri))
+		uri2 = open(uri, :allow_redirections => :safe).read
+		#source = Net::HTTP.get(uri)
+		source = uri2.split('\n')
+		temp = []
+		puts "hello?"
+		source.each do |line|
+			if line.include? "archives;" then 
+				temp << line
+			end
+		end
+		return source
+	}
 end
 
 
 
 #main
 
-nav_bar = return_pages("https://projecteuler.net/archives")
-puts "#{source}"
+nav_bar = return_pages('http://projecteuler.net/archives')
+puts "#{nav_bar}"
