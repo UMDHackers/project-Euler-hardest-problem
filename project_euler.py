3f19e811c616f0beca46f69f72afb021a4b6e54c
Parth Desai <pdesai@fc-md.umd.edu>
2015-12-04 12:58:43 -0500

    almost done

--- project_euler.py
+++ project_euler.py
@@ -1,4 +1,6 @@
 #C:/Phyton27/ 
+#find hardest problem on projecteuler, based on diffcult source code
+
 import re
 import urllib2
 from bs4 import BeautifulSoup
@@ -10,7 +12,7 @@ class Problem:
 		self.page = page
 		self.solved_by = solved_by
 	
-#Global
+#Globals
 problem_urls = dict()
 problems_list = list()
 def parseNavBar(rawCode):
@@ -46,22 +48,19 @@ def parseProblemPages(link):
 	problem_page = urllib2.urlopen('http://projecteuler.net/'+link)
 	pageX_html = problem_page.read()
 	soup = BeautifulSoup(pageX_html, 'html.parser')
-	link_number = re.search('problem=(\S+)', link)
+	link_number = re.search('problem=(\S+)', link).group(1)
 	for i in soup.findAll('span'):
 		if "Published" in str(i):
 			string_list = str(i).split(";")
-			print string_list[1]
-			print string_list[2]
-			
 			solved_by = re.search(' Solved by (\S+)', string_list[1]).group(1)
-			diffculty = re.search(' Difficulty rating: (\S+)%</span>', string_list[2])
-			if diffculty == None:
-				diffculty = re.search(' Difficulty rating: (\S+)% (Not yet finalised)</span>', string_list[2]).group(1)
+			diffculty = re.search(' Difficulty rating: (\S+)%', string_list[2]).group(1)
+			diffculty = int(diffculty)
+			page = re.search('archives;page=(\S+)',problem_urls[link])
+			if page:
+				page = page.group(1)
 			else:
-				diffculty = diffculty.group(1)
-			
-			page = problem_urls[link]
-			temp_pro = Problem(link_number, diffculty, page, diffculty)
+				page = "1"
+			temp_pro = Problem(link_number, diffculty, page, solved_by)
 			problems_list.append(temp_pro)
 
 	
@@ -79,6 +78,9 @@ for page in navbar:
 #remove duplicates
 page_links = list(set(page_links))
 
+#add the first page
+page_links.append("archives")
+
 #find all problem links
 for link in page_links:
 	parseProblemLinks(link)
@@ -86,5 +88,16 @@ for link in page_links:
 #make obj for each problem
 for prb in problem_urls.keys():
 	parseProblemPages(prb)
+
+problems_list.sort(key=lambda x: x.diffculty, reverse=True)
+print "Top 50 hardest problems"
+for i in range(0,50):
+	current = problems_list[i]
+	num = str(i + 1)
+	prb_num = current.number
+	page_num = current.page
+	diffculty = str(current.diffculty)
+	solved_by = current.solved_by
+	print "Number " + num + ", Problem Number "+ prb_num + ", Page number " + page_num +", diffculty " + diffculty + "%, Solved by " + solved_by
 
 
